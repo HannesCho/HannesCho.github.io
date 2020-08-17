675a1b295a2bf960289c148d0b9595b694e2e9c5
HannesCho <63042736+HannesCho@users.noreply.github.com>
2020-08-17 19:26:40 +0200

    weather.js

--- weather.js
+++ weather.js
@@ -5,7 +5,7 @@ const COORDS = "coords";
 
 function getWeather(lat, lon) {
   fetch(
-    `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
   )
     .then(function (response) {
       return response.json();
